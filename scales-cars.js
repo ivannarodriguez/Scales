3dd1b43e021f8cb492e6e7178886c679eaa04b9a
ivannarodriguez <imr6@students.calvin.edu>
2020-01-16 20:30:38 -0500

    added x and y axes

--- scales-cars.js
+++ scales-cars.js
@@ -5,57 +5,66 @@
 // let min = d3.min(cars.map(d => d.mpg))
 // let max = d3.max(cars.map(d => d.mpg))
 
-
 //adding a margin to the svg
-let margin = {top: 50, bottom: 50, left: 40, right: 40 };
-let width = 600, height = 400;
+let margin = {top: 5, bottom: 5, left: 20, right: 20 };
+let svgWidth = 700;
+let svgHeight = 500;
+let width = svgWidth - margin.left - margin.right;
+let height = svgHeight - margin.top - margin.bottom;
 
 // create svg for scatterplot to b at
-d3.select("body")
-  .append("svg")
-  .attr("id", "scatterplot")
-  .attr("width", width)
-  .attr("height", height)
-  .style('background-color', 'yellow')
-  .style('fill', "blue");
-  // .append("g")
-  //   .attr('id', 'scatterplotg')
-  //   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-//x scale is disp
+let canvas = d3.select("body")
+                .append("svg")
+                .attr("id", "scatterplot")
+                .attr("width", svgWidth)
+                .attr("height", svgHeight)
+                // .style('background-color', 'yellow')
+                .append("g")
+                  .attr('transform', "translate(" + margin.left + "," + margin.top + ")");
+/*
+ *  S C A L E S
+ */
+//horizontal scale is disp
 let dispscale = d3.scaleLinear()
   .domain(d3.extent(cars.map(d => d.disp)))
   .range([20, 570]); //remember the brackes! input is an array. this is a little less than the size of svg
-//y scale is mpg
+//vertical scale scale is mpg
 let mpgscale = d3.scaleLinear()
   .domain(d3.extent(cars.map(d => d.mpg)))
-  .range([370, 20]);
-
+  .range([370, 20]); //backwards because 0,0 is at top left corner
 //color scale
 let colorscale = d3.scaleQuantize()
   .domain(d3.extent(cars.map(d => d.cyl)))
   .range(["RebeccaPurple", "RosyBrown", "Teal", "Salmon"]);
-
 //size scale
 let sizescale = d3.scaleSqrt()
   .domain(d3.extent(cars.map(d => d.wt)))
   .range([3, 30]);
 
-// creating circles
-d3.selectAll('svg#scatterplot')
-  .selectAll('circle')
-  .data(cars) // include cars-data.js tho
-    .enter()
-    .append('circle')
-      .attr('cx', d => dispscale(d.disp)) // x axis scale
-      .attr('cy', d => mpgscale(d.mpg)) // y axis scale
-      .attr('r', d => sizescale(d.wt)) //size scale
-      .style('fill', d => colorscale(d.cyl)) //color scale
-      .style('opacity', 0.7)
-      .style('stroke', 'transparent');
-
-// add axis
-let xaxis = d3.axisBottom()
+// creating circles linked to our data
+// d3.selectAll('svg#scatterplot')
+let circles = canvas.selectAll('circle')
+                    .data(cars) // include cars-data.js tho
+                      .enter()
+                      .append('circle')
+                        .attr('cx', d => dispscale(d.disp)) // x axis scale
+                        .attr('cy', d => mpgscale(d.mpg)) // y axis scale
+                        .attr('r', d => sizescale(d.wt)) //size scale
+                        .style('fill', d => colorscale(d.cyl)) //color scale
+                        .style('opacity', 0.7)
+                        .style('stroke', 'transparent');
+/*
+ *  A X I S
+ */
+// add axis (disp is horizontal, mpg is vertical)
+let hAxis = d3.axisBottom()
               .scale(dispscale);
-d3.selectAll('svg#scatterplot')
-  .call(xaxis);
+let vAxis = d3.axisLeft()
+              .scale(mpgscale);
+//append g element for axis
+canvas.append("g")
+      .attr("transform", "translate(0,0)")
+      .call(vAxis);
+canvas.append("g")
+      .attr("transform", "translate(0"+","+370+")")
+      .call(hAxis);
